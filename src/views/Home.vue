1a5ef3bf46f22a0422575df54a7724194a7a7009
Evgeniy Tsymbalko <e_tsymbalko@wargaming.net>
2020-01-30 20:59:20 +0300

    Added stylelint in webpack

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="home" id="test">
-    this is home page
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci aperiam consequatur debi
+      tis facere fuga hic id laborum minus nam neque non optio praesentium qui repellat sed soluta
+      totam, velit voluptas?
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: "home"
-};
+  name: 'home'
+}
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,36 @@
-const path = require('path');
+const path = require("path");
+const StyleLintPlugin = require("stylelint-webpack-plugin");
 module.exports = {
   css: {
     requireModuleExtension: false,
     loaderOptions: {
       css: {
         modules: {
-          localIdentName: '[local]_[hash:base64:5]'
+          localIdentName: "[local]_[hash:base64:5]"
         },
-        localsConvention: 'camelCaseOnly'
+        localsConvention: "camelCaseOnly"
       }
     }
   },
   configureWebpack: {
     resolve: {
       alias: {
-        atoms: path.resolve('./src/components/atoms'),
-        molecules: path.resolve('./src/components/molecules'),
-        organisms: path.resolve('./src/components/organisms'),
-        pages: path.resolve('./src/components/pages'),
-        templates: path.resolve('./src/components/templates'),
-        assets: path.resolve('./src/assets')
+        components: path.resolve("./src/components"),
+        atoms: path.resolve("./src/components/atoms"),
+        molecules: path.resolve("./src/components/molecules"),
+        organisms: path.resolve("./src/components/organisms"),
+        pages: path.resolve("./src/components/pages"),
+        templates: path.resolve("./src/components/templates"),
+        assets: path.resolve("./src/assets")
       }
-    }
+    },
+    plugins: [
+      new StyleLintPlugin({
+        files: ["**/*.{vue,htm,html,css,sss,less,scss,sass}"],
+        context: "src",
+        configFile: ".stylelintrc",
+        lintDirtyModulesOnly: true
+      })
+    ]
   }
-}
+};
