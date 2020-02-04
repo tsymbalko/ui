f3cd488d17bf7ee84f38fb4c1d3ab0f758ac8da2
Evgeniy Tsymbalko <e_tsymbalko@wargaming.net>
2020-02-04 22:59:36 +0300

    Fix example

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,38 +4,45 @@
     <div style="margin-bottom: 20px;">
       Buttons has 4 states (warning, success, error, warning)
     </div>
-    <div class="example-spinner">
+    <div class="section-example">
+      <Tag>183%</Tag>
+      <Tag color="#3796f6">Text</Tag>
+      <Tag color="#ff790e">Text Value</Tag>
+      <Tag color="#ec547a">183%</Tag>
+      <Tag color="#5bceae">183%</Tag>
+    </div>
+    <div class="section-example">
       <Tag icon="archive-alt">183%</Tag>
       <Tag icon="archive-alt" color="#3796f6">Text</Tag>
       <Tag icon="archive-alt" color="#ff790e">Text Value</Tag>
       <Tag icon="archive-alt" color="#ec547a">183%</Tag>
       <Tag icon="archive-alt" color="#5bceae">183%</Tag>
     </div>
-    <div class="example-spinner">
+    <div class="section-example">
       <Tag closable icon="cog">183%</Tag>
       <Tag closable icon="cog" color="#3796f6">Text</Tag>
       <Tag closable icon="cog" color="#ff790e">Text Value</Tag>
       <Tag closable icon="cog" color="#ec547a">183%</Tag>
       <Tag closable icon="cog" color="#5bceae">183%</Tag>
     </div>
-    <div class="example-spinner">
-      <div class="buttons-example">
+    <div class="section-example">
+      <div class="section-example">
         <Spinner type="plane" />
         <Spinner type="bounce" />
         <Spinner type="pulse" />
       </div>
-      <div class="buttons-example">
+      <div class="section-example">
         <Spinner type="plane" color="#ff790e" />
         <Spinner type="bounce" color="#ff790e" />
         <Spinner type="pulse" color="#ff790e" />
       </div>
-      <div class="buttons-example">
+      <div class="section-example">
         <Spinner type="plane" color="#ec547a" />
         <Spinner type="bounce" color="#ec547a" />
         <Spinner type="pulse" color="#ec547a" />
       </div>
     </div>
-    <div class="buttons-example">
+    <div class="section-example">
       <Button type="primary">Add members</Button>
       <Button type="primary" icon="archive-alt" icon-position="left"
         >Add members</Button
@@ -49,7 +56,7 @@
         >Add members</Button
       >
     </div>
-    <div class="buttons-example">
+    <div class="section-example">
       <Button type="warning">Add members</Button>
       <Button icon="archive-alt" type="warning" icon-position="left"
         >Add members</Button
@@ -63,7 +70,7 @@
         >Add members</Button
       >
     </div>
-    <div class="buttons-example">
+    <div class="section-example">
       <Button type="error">Add members</Button>
       <Button icon="archive-alt" type="error" icon-position="left"
         >Add members</Button
@@ -75,7 +82,7 @@
         >Add members</Button
       >
     </div>
-    <div class="buttons-example">
+    <div class="section-example">
       <Button type="success">Add members</Button>
       <Button icon="archive-alt" type="success" icon-position="left"
         >Add members</Button
@@ -89,7 +96,7 @@
         >Add members</Button
       >
     </div>
-    <div class="buttons-example">
+    <div class="section-example">
       <Button type="success" disabled>Add members</Button>
       <Button icon="archive-alt" type="success" icon-position="left" disabled
         >Add members</Button
@@ -105,7 +112,7 @@
         >Add members</Button
       >
     </div>
-    <div class="buttons-example">
+    <div class="section-example">
       <Button>Add members</Button>
       <Button icon="archive-alt">Add members</Button>
       <Button icon="archive-alt" icon-position="left">Add members</Button>
@@ -114,7 +121,7 @@
       <Button icon="archive-alt" shape="round">Add members</Button>
       <Button icon="archive-alt" shape="circle">Add members</Button>
     </div>
-    <div class="buttons-example">
+    <div class="section-example">
       <Button :loading="loading" type="primary">Add members</Button>
       <Button :loading="loading" type="primary" icon="archive-alt"
         >Add members</Button
@@ -137,7 +144,7 @@
         >Add members</Button
       >
     </div>
-    <div class="buttons-example">
+    <div class="section-example">
       <Button :loading="loading" type="warning">Add members</Button>
       <Button :loading="loading" icon="archive-alt" type="warning"
         >Add members</Button
@@ -186,26 +193,17 @@ export default {
 </script>
 
 <style lang="scss">
-.example-spinner {
-  display: flex;
-  margin-bottom: 20px;
-
-  & > div {
-    margin-right: 20px;
-  }
-}
-
-.buttons-example {
+.section-example {
   display: flex;
   align-items: center;
   margin-bottom: 34px;
 
   & > button {
-    margin-right: 14px;
+    margin-right: 16px;
   }
 
   & > .spinner {
-    margin-right: 10px;
+    margin-right: 16px;
   }
 
   & > .tag {
