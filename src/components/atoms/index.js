83f902ab73f8946638c3a8b408e122cf796c7712
Evgeniy Tsymbalko <e_tsymbalko@wargaming.net>
2020-02-05 11:02:23 +0300

    Refactoring switch

--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,5 +1,5 @@
 import Icon from './Icon/Icon'
-import VSwitch from './VSwitch/VSwitch'
+import VSwitch from './Switch/VSwitch'
 import Color from './Color/Color'
 import Button from './Button/Button'
 import Spinner from './Spinner/Spinner'
