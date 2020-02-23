aef52d70fa7923c1b986ec806a09be029ebe441c
Evgeniy Tsymbalko <e_tsymbalko@wargaming.net>
2020-02-23 17:38:13 +0300

    Added animation modal

--- src/components/molecules/Modal/Modal.vue
+++ src/components/molecules/Modal/Modal.vue
@@ -1,52 +1,54 @@
 <template>
-  <div v-if="visible" class="vc-modal">
-    <div class="vc-modal_mask">
-      <div
-        tabindex="-1"
-        class="vc-modal_content vc-modal_content--md"
-        role="dialog"
-        v-click-outside="close"
-        style="transform: translate(-50%, 0);"
-      >
-        <Trap>
-          <div class="vc-modal_header">
-            <h2 class="vc-modal_title">Simple title</h2>
-            <Button
-              ref="modalClose"
-              class="vc-modal__close-btn"
-              icon="multiply"
-              shape="square"
-              type="ghost"
-              aria-label="close modal"
-              @click="$emit('close', false)"
-            />
-          </div>
-          <div class="vc-modal_body">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum in
-            obcaecati optio pariatur perferendis porro quasi quisquam soluta
-            vero, vitae voluptas voluptate. Accusantium dolor dolorem, molestias
-            neque quidem tenetur? Soluta? Lorem ipsum dolor sit amet,
-            consectetur adipisicing elit. Accusamus adipisci consequuntur
-            debitis, deserunt ea esse expedita fuga inventore ipsam itaque
-            necessitatibus nesciunt nisi nostrum obcaecati perferendis provident
-            quia quod tempora! Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit. Atque delectus id minus quisquam reiciendis
-            reprehenderit sapiente tempora tenetur! Aperiam esse iure minima
-            necessitatibus nisi odit possimus ratione similique voluptas
-            voluptatem.
-          </div>
-          <div class="vc-modal_footer">
-            <Button type="bordered" @click="$emit('close', false)">
-              Cancel
-            </Button>
-            <Button icon="archive-alt" type="primary">
-              Submit
-            </Button>
-          </div>
-        </Trap>
+  <transition name="vc-modal__animation">
+    <div v-if="visible" class="vc-modal">
+      <div class="vc-modal_mask">
+        <div
+          tabindex="-1"
+          class="vc-modal_content vc-modal_content--md"
+          role="dialog"
+          v-click-outside="closeModal"
+          style="transform: translate(-50%, 0);"
+        >
+          <Trap>
+            <div class="vc-modal_header">
+              <h2 class="vc-modal_title">Simple title</h2>
+              <Button
+                ref="modalClose"
+                class="vc-modal__close-btn"
+                icon="multiply"
+                shape="square"
+                type="ghost"
+                aria-label="close modal"
+                @click="closeModal"
+              />
+            </div>
+            <div class="vc-modal_body">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum in
+              obcaecati optio pariatur perferendis porro quasi quisquam soluta
+              vero, vitae voluptas voluptate. Accusantium dolor dolorem,
+              molestias neque quidem tenetur? Soluta? Lorem ipsum dolor sit
+              amet, consectetur adipisicing elit. Accusamus adipisci
+              consequuntur debitis, deserunt ea esse expedita fuga inventore
+              ipsam itaque necessitatibus nesciunt nisi nostrum obcaecati
+              perferendis provident quia quod tempora! Lorem ipsum dolor sit
+              amet, consectetur adipisicing elit. Atque delectus id minus
+              quisquam reiciendis reprehenderit sapiente tempora tenetur!
+              Aperiam esse iure minima necessitatibus nisi odit possimus ratione
+              similique voluptas voluptatem.
+            </div>
+            <div class="vc-modal_footer">
+              <Button type="bordered" @click="closeModal">
+                Cancel
+              </Button>
+              <Button icon="archive-alt" type="primary">
+                Submit
+              </Button>
+            </div>
+          </Trap>
+        </div>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
@@ -84,7 +86,7 @@ export default {
   methods: {
     handleEscapeKey(e) {
       if (e.code === 'Escape') {
-        this.$emit('close', false)
+        this.closeModal()
         e.preventDefault()
       }
     },
@@ -95,7 +97,7 @@ export default {
         this.$refs.modalClose.$el.focus()
       })
     },
-    close() {
+    closeModal() {
       this.$emit('close', false)
     }
   },
