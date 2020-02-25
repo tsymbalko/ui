ef948efadd259f931f2d98b9c607d5b0d5943b9d
Evgeniy Tsymbalko <e_tsymbalko@wargaming.net>
2020-02-25 22:27:12 +0300

    Fix modal

--- src/components/molecules/Modal/Modal.vue
+++ src/components/molecules/Modal/Modal.vue
@@ -1,58 +1,63 @@
 <template>
   <transition name="vc-modal__animation">
-    <div v-if="visible" class="vc-modal">
-      <div class="vc-modal_mask">
-        <div
-          tabindex="-1"
-          class="vc-modal_content vc-modal_content--md"
-          role="dialog"
-          v-click-outside="closeModal"
-          style="transform: translate(-50%, 0);"
-        >
-          <Trap>
-            <div class="vc-modal_header">
-              <h2 class="vc-modal_title">Simple title</h2>
-              <Button
-                ref="modalClose"
-                class="vc-modal__close-btn"
-                icon="multiply"
-                shape="square"
-                type="ghost"
-                aria-label="close modal"
-                @click="closeModal"
-              />
-            </div>
-            <div class="vc-modal_body">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum in
-              obcaecati optio pariatur perferendis porro quasi quisquam soluta
-              vero, vitae voluptas voluptate. Accusantium dolor dolorem,
-              molestias neque quidem tenetur? Soluta? Lorem ipsum dolor sit
-              amet, consectetur adipisicing elit. Accusamus adipisci
-              consequuntur debitis, deserunt ea esse expedita fuga inventore
-              ipsam itaque necessitatibus nesciunt nisi nostrum obcaecati
-              perferendis provident quia quod tempora! Lorem ipsum dolor sit
-              amet, consectetur adipisicing elit. Atque delectus id minus
-              quisquam reiciendis reprehenderit sapiente tempora tenetur!
-              Aperiam esse iure minima necessitatibus nisi odit possimus ratione
-              similique voluptas voluptatem.
-            </div>
-            <div class="vc-modal_footer">
-              <Button type="bordered" @click="closeModal">
-                Cancel
-              </Button>
-              <Button icon="archive-alt" type="primary">
-                Submit
-              </Button>
-            </div>
-          </Trap>
-        </div>
+    <div
+      v-if="visible"
+      :class="[
+        'vc-modal',
+        { 'vc-modal__mask': mask },
+        { 'vc-modal__center': center },
+        { 'vc-modal__fullscreen': fullscreen }
+      ]"
+    >
+      <div
+        class="vc-modal_content"
+        role="dialog"
+        v-click-outside="closeModal"
+        :style="{ '--max-width': maxWidth }"
+      >
+        <FocusLock :no-focus-guards="true">
+          <div class="vc-modal_header">
+            <h2 class="vc-modal_title">Simple title</h2>
+            <Button
+              ref="modalClose"
+              class="vc-modal__close-btn"
+              icon="multiply"
+              shape="square"
+              type="ghost"
+              aria-label="close modal"
+              @click="closeModal"
+            />
+          </div>
+          <div class="vc-modal_body">
+            e consectetur consequatur culpa enim incidunt laboriosam officiis
+            recusandae sit. Lorem ipsum dolor sit amet, consectetur adipisicing
+            elit. Accusantium at distinctio eaque ullam vero. Asperiores beatae,
+            excepturi fugiat hic officia reiciendis rerum sint. Dolorum eos
+            illum incidunt laboriosam nam officia. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Aspernatur blanditiis commodi
+            corporis, ea eos ipsa minus, possimus quaerat quam quibusdam rerum,
+            sapiente sunt! Commodi enim harum, magnam qui tempora ut? Lorem
+            ipsum dolor sit amet, consectetur adipisicing elit. Animi aut
+            consectetur, consequuntur ea enim est hic incidunt labore,
+            laboriosam maiores nulla obcaecati quam quas quod sunt totam veniam!
+            Ipsa, quaerat.
+          </div>
+          <div class="vc-modal_footer">
+            <Button type="bordered" @click="closeModal">
+              Cancel
+            </Button>
+            <Button icon="archive-alt" type="primary">
+              Submit
+            </Button>
+          </div>
+        </FocusLock>
       </div>
     </div>
   </transition>
 </template>
 
 <script>
-import Trap from 'vue-focus-lock'
+import FocusLock from 'vue-focus-lock'
 import vClickOutside from 'v-click-outside'
 import { Button } from 'atoms'
 import { LayoutLock } from 'mixins'
@@ -60,7 +65,7 @@ import { LayoutLock } from 'mixins'
 export default {
   components: {
     Button,
-    Trap
+    FocusLock
   },
   directives: {
     clickOutside: vClickOutside.directive
@@ -70,6 +75,22 @@ export default {
     visible: {
       type: Boolean,
       default: false
+    },
+    mask: {
+      type: Boolean,
+      default: true
+    },
+    center: {
+      type: Boolean,
+      default: false
+    },
+    maxWidth: {
+      type: String,
+      default: '650px'
+    },
+    fullscreen: {
+      type: Boolean,
+      default: true
     }
   },
   watch: {
