749048e171de090ea0955b1d6813abc348522ff6
Evgeniy Tsymbalko <e_tsymbalko@wargaming.net>
2020-03-05 23:22:45 +0300

    Fix profile page

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -7,50 +7,89 @@
           'background-image': `url(${require('assets/images/background.jpeg')})`
         }"
       />
-      <div class="profile_header-row">
-        <Avatar
-          class="profile_header-avatar"
-          :src="`${require('assets/images/avatar.png')}`"
-          :size="140"
-          shape="square"
-        />
-        <div class="profile_header-details">
-          <Heading divider="right">
-            <Indicator type="error">
-              Profile
-            </Indicator>
-          </Heading>
-          <TextLabel>
-            You have
-            <Badge :count="101" :overflow-count="999" type="error" />
-            messages
-          </TextLabel>
-        </div>
+      <Avatar
+        class="profile_header-avatar"
+        :src="`${require('assets/images/avatar.png')}`"
+        :size="160"
+        shape="square"
+      />
+      <div class="profile_header-details">
+        <Heading divider="right">
+          <Indicator type="error">
+            Mr. Bean
+          </Indicator>
+        </Heading>
+        <p>
+          <span class="profile_header-text">UI/UX Designer@spotify</span>
+          <span class="profile_header-text">
+            <Icon
+              width="18"
+              height="18"
+              name="map-marker"
+              fill="var(--orange)"
+              class="profile_icon"
+            />
+            Minsk
+          </span>
+        </p>
+        <TextLabel>
+          You have
+          <Badge :count="101" :overflow-count="999" type="error" />
+          messages
+        </TextLabel>
       </div>
     </div>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
-      explicabo ipsam laborum molestiae nam. A ab adipisci atque enim hic
-      laborum molestiae optio quaerat qui, quis rem sapiente sequi unde. Lorem
-      ipsum dolor sit amet, consectetur adipisicing elit. At blanditiis
-      doloremque, dolorum, eaque eligendi error explicabo illo laboriosam nulla
-      odio perspiciatis quas quia rerum sapiente temporibus unde vel voluptatem
-      voluptatibus.
-    </p>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
-      explicabo ipsam laborum molestiae nam. A ab adipisci atque enim hic
-      laborum molestiae optio quaerat qui, quis rem sapiente sequi unde. Lorem
-      ipsum dolor sit amet, consectetur adipisicing elit. At blanditiis
-      doloremque, dolorum, eaque eligendi error explicabo illo laboriosam nulla
-      odio perspiciatis quas quia rerum sapiente temporibus unde vel voluptatem
-      voluptatibus.
-    </p>
+    <div class="profile_body">
+      <div class="profile_body-main">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
+        explicabo ipsam laborum molestiae nam. A ab adipisci atque enim hic
+        laborum molestiae optio quaerat qui, quis rem sapiente sequi unde. Lorem
+        ipsum dolor sit amet, consectetur adipisicing elit. At blanditiis
+        doloremque, dolorum, eaque eligendi error explicabo illo laboriosam
+        nulla odio perspiciatis quas quia rerum sapiente temporibus unde vel
+        voluptatem voluptatibus. Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Ab aliquam architecto consequuntur debitis doloremque,
+        ducimus eligendi enim error est excepturi itaque magni nemo, quam rem
+        repellat soluta tempora velit, voluptate? Lorem ipsum dolor sit amet,
+        consectetur adipisicing elit. Debitis, quisquam, repellat. Ab eveniet
+        harum minus molestias, nam numquam omnis, pariatur quam quas quasi
+        quisquam reiciendis, repellat sunt tempore tenetur veniam? Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit. Consectetur explicabo
+        ipsam laborum molestiae nam. A ab adipisci atque enim hic laborum
+        molestiae optio quaerat qui, quis rem sapiente sequi unde. Lorem ipsum
+        dolor sit amet, consectetur adipisicing elit. At blanditiis doloremque,
+        dolorum, eaque eligendi error explicabo illo laboriosam nulla odio
+        perspiciatis quas quia rerum sapiente temporibus unde vel voluptatem
+        voluptatibus. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Ab aliquam architecto consequuntur debitis doloremque, ducimus eligendi
+        enim error est excepturi itaque magni nemo, quam rem repellat soluta
+        tempora velit, voluptate? Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Debitis, quisquam, repellat. Ab eveniet harum minus
+        molestias, nam numquam omnis, pariatur quam quas quasi quisquam
+        reiciendis, repellat sunt tempore tenetur veniam?
+      </div>
+      <div class="profile_body-aside">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur
+        explicabo ipsam laborum molestiae nam. A ab adipisci atque enim hic
+        laborum molestiae optio quaerat qui, quis rem sapiente sequi unde. Lorem
+        ipsum dolor sit amet, consectetur adipisicing elit. At blanditiis
+        doloremque, dolorum, eaque eligendi error explicabo illo laboriosam
+        nulla odio perspiciatis quas quia rerum sapiente temporibus unde vel
+        voluptatem voluptatibus. Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Dignissimos dolores ipsam necessitatibus obcaecati
+        perspiciatis ratione temporibus! Beatae dolorum expedita facere modi
+        necessitatibus nihil obcaecati porro praesentium quasi, quis
+        reprehenderit veniam! Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Blanditiis dignissimos error natus omnis quas suscipit
+        tempora voluptatem? Asperiores deserunt, eum exercitationem laboriosam
+        modi molestias quaerat quo repudiandae. Delectus libero, nobis.
+      </div>
+    </div>
   </Card>
 </template>
 
 <script>
-import { Heading, Indicator, TextLabel, Badge, Card, Avatar } from 'atoms'
+import { Heading, Indicator, TextLabel, Badge, Card, Avatar, Icon } from 'atoms'
 export default {
   components: {
     Heading,
@@ -58,7 +97,8 @@ export default {
     Indicator,
     TextLabel,
     Card,
-    Avatar
+    Avatar,
+    Icon
   }
 }
 </script>
