fd7367e05bea5ec2aba09cf90f7bab141887bdb0
tsymbalko <eugene.tsymbalko@gmail.com>
2020-03-12 23:49:37 +0300

    Progress bar

--- src/components/atoms/ProgressCircle/ProgressCircle.vue
+++ src/components/atoms/ProgressCircle/ProgressCircle.vue
@@ -2,14 +2,48 @@
   <div class="vc-progress-circle">
     <svg class="vc-progress-circle_box" viewBox="0 0 100 100">
       <circle class="vc-progress-circle_underlay" cx="50" cy="50" r="48" />
-      <circle class="vc-progress-circle_overlay" cx="50" cy="50" r="48" />
+      <circle
+        ref="circle"
+        class="vc-progress-circle_overlay"
+        cx="50"
+        cy="50"
+        r="48"
+        :style="{
+          strokeDasharray: `${pathLength}`,
+          strokeDashoffset: `${progress}`
+        }"
+      />
     </svg>
   </div>
 </template>
 
 <script>
 export default {
-  computed: {
+  data() {
+    return {
+      progress: 0,
+      pathLength: 0,
+      value: 10
+    }
+  },
+  watch: {
+    value() {
+      this.getValue()
+    }
+  },
+  methods: {
+    getValue() {
+      this.progress = this.pathLength - (this.value * this.pathLength) / 100
+    }
+  },
+  mounted() {
+    //eslint-disable-next-line
+    console.log(this.$refs.circle.getTotalLength())
+    this.pathLength = this.$refs.circle.getTotalLength()
+    this.getValue()
+    setTimeout(() => {
+      this.value = 30
+    }, 10000)
     // normalizedValue (): number {
     //   if (this.value < 0) {
     //     return 0
